0244d3038b58fc4c1356290a0205347e838e3628
שם המשתמש שלך <your-email@example.com>
2025-05-27 10:03:17 +0300

    add attack func

--- comndos proj/Program.cs
+++ comndos proj/Program.cs
@@ -10,33 +10,46 @@ namespace MyNamespace
         walking,
         hiding
     }
+
     public class Commando
     {
-       private string Name { get; set; }
-       private string CodeName { get; set; }
-       private string[] Tools = new string[] { "Hammer", "Chisel", "Rope","Bag","Watery" };
-       private Status Status { get; set; }
+        private string Name { get; set; } = "papa";
+        private string CodeName { get; set; } = "x";
+        private string[] Tools = new string[] { "Hammer", "Chisel", "Rope", "Bag", "Watery" };
+        private Status Status { get; set; }
 
 
-       public Commando(string name, string codeName, string[] tools, Status status)
-       {
-           this.Name = name;
-           this.CodeName = codeName;
-           this.Tools = tools;
-           this.Status = status;
-       }
+        public Commando(string name, string codeName, string[] tools, Status status)
+        {
+            this.Name = name;
+            this.CodeName = codeName;
+            this.Tools = tools;
+            this.Status = status;
+        }
 
-       public void Walk()
-       {
-           this.Status = Status.walking;
-           Console.WriteLine("The soldier is walking");
-       }
+        public void Walk()
+        {
+            this.Status = Status.walking;
+            Console.WriteLine("The soldier is walking");
+        }
 
-       public void Hide()
-       {
-           this.Status = Status.hiding;
-           Console.WriteLine("The soldier is hiding");
-       }
+        public void Hide()
+        {
+            this.Status = Status.hiding;
+            Console.WriteLine("The soldier is hiding");
+        }
+
+        public void Attack(string input)
+        {
+            if (input == "codename")
+            {
+                Console.WriteLine($"Commando with code name {this.CodeName} is attacking");
+            }
+            else if (input == "name")
+            {
+                Console.WriteLine($"Commando name is {this.Name}");
+            }
+        }
+
     }
 }
-
