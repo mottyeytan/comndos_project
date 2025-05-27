a30ab1f8d720149b98fc6adb8e5f46bc9e0fd773
שם המשתמש שלך <your-email@example.com>
2025-05-27 09:48:30 +0300

    add constrctor and ealk func

---
+++ comndos proj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+namespace MyNamespace
+{
+    public enum Status
+    {
+        standing,
+        walking,
+        hiding
+    }
+    public class Commando
+    {
+       private string Name { get; set; }
+       private string CodeName { get; set; }
+       private string[] Tools = new string[] { "Hammer", "Chisel", "Rope","Bag","Watery" };
+       private Status Status { get; set; }
+
+
+       public Commando(string name, string codeName, string[] tools, Status status)
+       {
+           this.Name = name;
+           this.CodeName = codeName;
+           this.Tools = tools;
+           this.Status = status;
+       }
+
+       public void Walk()
+       {
+           this.Status = Status.walking;
+           Console.WriteLine("The soldier is walking");
+       }
+       
+       public voud 
+    }
+}
+
